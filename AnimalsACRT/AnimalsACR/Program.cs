7ecb7814bed9cc476c6dccf8c07e541533c01a80
alecurobert05 <infoalecu@gmail.com>
2019-06-07 21:07:27 +0300

    Update Program.cs
    Owner of Animals show
    Shelter Id show
    Number of Animals show

--- AnimalsACRT/AnimalsACR/Program.cs
+++ AnimalsACRT/AnimalsACR/Program.cs
@@ -16,6 +16,14 @@ namespace AnimalsACR
 
             Console.WriteLine("The cat is named {0} and says {1}",cat.GetName(), cat.Sound);
 
+            cat.Owner = "Alecu";
+
+            Console.WriteLine("{0} owner is {1}", cat.GetName(), cat.Owner);
+
+            Console.WriteLine("{0} Shelter id is {1}", cat.GetName(), cat.idNum);
+
+            Console.WriteLine("# of Animals : {0}", Animals.NumOfAnimals);
+
             Console.ReadLine();
         }
     }
