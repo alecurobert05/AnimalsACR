a1549ab7a03214b71fefbe5e3fb28838c93c7921
alecurobert05 <infoalecu@gmail.com>
2019-06-07 19:03:08 +0300

    Added 1 animal
    I added 1 animal with one  sound.

---
+++ AnimalsACRT/AnimalsACR/Animals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace AnimalsACR
+{
+    class Animals
+    {
+        public string name;
+        public string sound;
+
+        public Animals()
+        {
+            name = "No Name";
+            sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animals(string name = "No Name")
+        {
+            this.name = name;
+            this.sound = "No Sound";
+            numOfAnimals++;
+        }
+
+        public Animals(string name = "No Name", string sound = "No Sound")
+        {
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+        
+        static int numOfAnimals = 0;
+
+        public static int GetNumAnimals()
+        {
+            return numOfAnimals;
+        }
+
+    }
+}
